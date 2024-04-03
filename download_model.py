1f4dbf1921a0864d2ade5b759d7fc53bed8194e2
Christopher Davis <chris@pmg.com>
2024-04-03 09:40:56 -0500

    Add requirements.txt

--- download_model.py
+++ download_model.py
@@ -10,14 +10,14 @@ from transformers import AutoTokenizer, AutoModel
 
 ArgsType = typing.Optional[typing.List[str]]
 
-def _parse_args(args: ArgsType=None) -> argparse.Namespace:
+def parse_args(args: ArgsType=None) -> argparse.Namespace:
     p = argparse.ArgumentParser()
     p.add_argument('model_name', help='The model name to download')
     p.add_argument('output_path', type=pathlib.Path, help='where to save the model')
 
     return p.parse_args(args)
 
-def _download_model(model_name: str, output_path: pathlib.Path):
+def download_model(model_name: str, output_path: pathlib.Path):
     """
     Could drop this into a notebook to download the model in a specific
     environment (eg AWS sagemaker with inferentia)
@@ -32,9 +32,9 @@ def _download_model(model_name: str, output_path: pathlib.Path):
     model.save_pretrained(str(output_path))
 
 def main(args: ArgsType=None):
-    args = _parse_args(args)
+    args = parse_args(args)
 
-    _download_model(args.model_name, args.output_path)
+    download_model(args.model_name, args.output_path)
 
 
 if __name__ == '__main__':
