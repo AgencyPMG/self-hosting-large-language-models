ac1f010e3847d0116e7ae6695238511e990f8ae1
Christopher Davis <chris@pmg.com>
2024-04-24 05:32:14 -0500

    Remove PMG's State from the Repo
    Nothing sensitive here, just making it easier for others to copy.

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = ">= 1.6"
 
-  backend "s3" {
-    bucket     = "playground-infrastructure"
-    key        = "selfhostingtalk/main.tfstate"
-    region     = "us-east-1"
-    encrypt    = true
-    kms_key_id = "arn:aws:kms:us-east-1:625506553848:key/99fa0cc4-d366-4857-a34f-a3fb9be28382"
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
